f1e899f1654de9ce1559c9b0ac9a14631879706e
TheUnproductive <geil.js18@gmail.com>
2022-05-10 21:04:20 +0200

    Create decoder

---
+++ decodewav.py
@@ -0,0 +1,44 @@
+## This is a WIP file
+
+import wavio
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description="Start DoorPi with certain options")
+parser.add_argument("-r", action="store", dest='rate', type=int, default=44100,
+	help="Set sampling rate")	# samples per second, every second 44100 samples are used, for 100ms --> 44100/(1000/100)
+parser.add_argument("-n", action="store", dest='name', type=str, default="h",
+    help="Manually configure filename")
+parser.add_argument("-d", action="store", dest="data", type=str,
+    help="Input data to transform")
+args = parser.parse_args()
+
+rate = args.rate
+filename = args.name
+
+T = 1         # sample duration for each bit (seconds), can be changed using the ms down below
+f1 = 1200.0   # sound frequency (Hz) for 0 bit
+f2 = 44000.0   # sound frequency (Hz) for 1 bit
+start_sequence = 1800.0   # start frequencies
+stop_sequence = 3600.0    # stop frequency
+
+
+ms = 10; #milliseconds between each bit
+samples = rate//(1000//ms)
+
+if f1 > rate//2 or f2 > rate//2:
+	print("Error, maximum frequency exceeds " + str(rate/2))
+	exit()
+
+if args.data:
+	s = args.data
+else:
+	s = input("Enter your data: ")
+	s += '.'
+
+print("Input Stream: " + (''.join(map(bin,bytearray(s, 'utf-8')))) + "\n")
+binout = ((''.join(map(bin,bytearray(s, 'utf-8')))).replace("b", "")).replace(" ", "")
+print("Binout: %s \n" % (binout))
+
+x = []
+l = []
