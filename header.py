95f268ca1443cf60d94c2b983a9bd7735faa7c57
justu <geil.js18@gmail.com>
2022-05-11 09:07:51 +0200

    Create new function, ease of use

--- header.py
+++ header.py
@@ -12,57 +12,30 @@ def stop(T, rate, x, stop_sequence, samples):
 	t = np.linspace(0, T, T*rate, endpoint=False)
 	x.append(np.sin(2*np.pi * stop_sequence * t[:samples]))
 
-def short(T, rate, x, f1, f2, samples):
-	binpref = ((''.join(map(bin,bytearray("s", 'utf-8')))).replace("b", "")).replace(" ", "")
+def writeheaderdata(T, rate, x, f1, f2, samples, string):
+	binpref = ((''.join(map(bin,bytearray(string, 'utf-8')))).replace("b", "")).replace(" ", "")
 	for i in binpref:
 		t = np.linspace(0, T, T*rate, endpoint=False)
 		if i == '0':
 			x.append(np.sin(2*np.pi * f1 * t[:samples]))
 		if i == '1':
 			x.append(np.sin(2*np.pi * f2 * t[:samples]))
+
+def short(T, rate, x, f1, f2, samples):
+	writeheaderdata(T, rate, x, f1, f2, samples, "s")
 
 def standard(T, rate, x, f1, f2, samples):
-	binpref = ((''.join(map(bin,bytearray("standard", 'utf-8')))).replace("b", "")).replace(" ", "")
-	for i in binpref:
-		t = np.linspace(0, T, T*rate, endpoint=False)
-		if i == '0':
-			x.append(np.sin(2*np.pi * f1 * t[:samples]))
-		if i == '1':
-			x.append(np.sin(2*np.pi * f2 * t[:samples]))
+	writeheaderdata(T, rate, x, f1, f2, samples, "standard")
 
-def custom(T, rate, x, f1, f2, file, samples):
-	binpref = ((''.join(map(bin,bytearray("custom", 'utf-8')))).replace("b", "")).replace(" ", "")
-	for i in binpref:
-		t = np.linspace(0, T, T*rate, endpoint=False)
-		if i == '0':
-			x.append(np.sin(2*np.pi * f1 * t[:samples]))
-		if i == '1':
-			x.append(np.sin(2*np.pi * f2 * t[:samples]))
-	binpref = ((''.join(map(bin,bytearray(str(f1), 'utf-8')))).replace("b", "")).replace(" ", "")
-	for i in binpref:
-		t = np.linspace(0, T, T*rate, endpoint=False)
-		if i == '0':
-			x.append(np.sin(2*np.pi * f1 * t[:samples]))
-		if i == '1':
-			x.append(np.sin(2*np.pi * f2 * t[:samples]))
-	binpref = ((''.join(map(bin,bytearray(str(f2), 'utf-8')))).replace("b", "")).replace(" ", "")
-	for i in binpref:
-		t = np.linspace(0, T, T*rate, endpoint=False)
-		if i == '0':
-			x.append(np.sin(2*np.pi * f1 * t[:samples]))
-		if i == '1':
-			x.append(np.sin(2*np.pi * f2 * t[:samples]))
-	binpref = ((''.join(map(bin,bytearray(str(rate), 'utf-8')))).replace("b", "")).replace(" ", "")
-	for i in binpref:
-		t = np.linspace(0, T, T*rate, endpoint=False)
-		if i == '0':
-			x.append(np.sin(2*np.pi * f1 * t[:samples]))
-		if i == '1':
-			x.append(np.sin(2*np.pi * f2 * t[:samples]))
-	binpref = ((''.join(map(bin,bytearray(str(file), 'utf-8')))).replace("b", "")).replace(" ", "")
-	for i in binpref:
-		t = np.linspace(0, T, T*rate, endpoint=False)
-		if i == '0':
-			x.append(np.sin(2*np.pi * f1 * t[:samples]))
-		if i == '1':
-			x.append(np.sin(2*np.pi * f2 * t[:samples]))
+def custom(T, rate, x, f1, f2, file, samples, ms):
+	writeheaderdata(T, 44100, x, 2.0, 22048.0, samples, "custom")
+
+	writeheaderdata(T, 44100, x, 2.0, 22048.0, samples, str(f1))
+
+	writeheaderdata(T, 44100, x, 2.0, 22048.0, samples, str(f2))
+
+	writeheaderdata(T, 44100, x, 2.0, 22048.0, samples, str(rate))
+
+	writeheaderdata(T, 44100, x, 2.0, 22048.0, samples, str(ms))
+
+	writeheaderdata(T, 44100, x, 2.0, 22048.0, samples, str(file))
