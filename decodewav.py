15b6cb701055aa65795b75aeba30b1784a357c17
justu <geil.js18@gmail.com>
2022-05-12 15:39:24 +0200

    Add header analysis

--- decodewav.py
+++ decodewav.py
@@ -42,11 +42,46 @@ for i, chunk in enumerate(in_data[::ms]):
 print(output)
 
 for i, chunk in enumerate(in_data[::ms]):
-  with open("sounds/sound-%s.wav" % i, "wb") as f:
+  with open("sounds/%s-sound.wav" % i, "wb") as f:
     chunk.export(f, format="wav")
 
 files = os.listdir("sounds")
+files.sort()
 out_list = []
+
+def analyzeHead():
+    # read until second start frequence
+    for i in files:
+        filename = "sounds/" + i
+        fs_rate, signal = wavfile.read(filename)
+
+        print ("Frequency sampling", fs_rate)
+        l_audio = len(signal.shape)
+        print ("Channels", l_audio)
+        if l_audio == 2:
+            signal = signal.sum(axis=1) / 2
+        N = signal.shape[0]
+        print ("Complete Samplings N", N)
+        secs = N / float(fs_rate)
+        print ("secs", secs)
+        Ts = 1.0/fs_rate # sampling interval in time
+        print ("Timestep between samples Ts", Ts)
+        t = np.arange(0, secs, Ts) # time vector as scipy arange field / numpy.ndarray
+        FFT = scipy.fftpack.fft(signal)
+        FFT = abs(FFT)
+        FFT_side = FFT[range(N//2)] # one side FFT range
+        FFT_side = FFT_side[0:10]
+        freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
+        fft_freqs = np.array(freqs)
+        freqs_side = freqs[range(N//2)] # one side frequency range
+        fft_freqs_side = np.array(freqs_side)
+        #np.delete(FFT_side, 0, None)
+        out_list.append(np.argmax(FFT_side)) # get index of maximum value
+
+        os.system("rm " + filename)
+    
+    # write stuff from header to variables
+
 for i in files:
     filename = "sounds/" + i
     fs_rate, signal = wavfile.read(filename)
